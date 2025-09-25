76364b68292a46acbcfbda60f544c76563acfc1a
conicorreag <conicorreag@uc.cl>
2025-09-25 18:05:08 -0300

    chore: changes to test CI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./styles/App.css";
 function App() {
   return (
     <>
-      <h1>Proyecto QR UC CHRISTUS Testeando CI</h1>
+      <h1>Proyecto QR UC CHRISTUS Testeando CI!!</h1>
     </>
   );
 }
