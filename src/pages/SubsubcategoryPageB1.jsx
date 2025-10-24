a249c2832dacb4413d1da7d1ff8fc473b50f8974
amandaarod <93745031+amandaarod@users.noreply.github.com>
2025-10-24 18:21:28 -0300

    fix linter SubsubcategoryPageB1.jsx

--- src/pages/SubsubcategoryPageB1.jsx
+++ src/pages/SubsubcategoryPageB1.jsx
@@ -2,11 +2,9 @@ import { Header } from "../components/Header";
 import { Button } from "../components/Button";
 import { Footer } from "../components/Footer";
 import { useQrStore } from "../stores/QRStore.js";
-import { useParams } from "react-router-dom";
 
 export function SubsubategoryPageB1() {
   const { qrData } = useQrStore();
-  const { id } = useParams();
   return (
     <div className="container">
       <Header title={"INFORMACIÓN GES - CAEC - LEY DE URGENCIA"}></Header>
